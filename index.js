377517f1e52c22b4a627e0ecb5d717ec47959e71
Roberto Esteves <contact@robertoesteves.dev>
2024-05-06 09:40:57 -0300

    configure cors

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
